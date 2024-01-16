4ffacc00b23368b0c06ce28b06b2b8d753a3d702
mbp13 <shaxobiddina20@gmail.com>
2024-01-16 20:47:55 +0500

    deploy git

--- rollhall/urls.py
+++ rollhall/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
